4091d94b30d4f8af6f2daae1a0b4e2ec8478f1f4
Zain Jaafar <zainjaafar120@gmail.com>
2024-11-09 14:35:34 +1300

    added margin

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ export default function Button(props) {
     const { children } = props
     return (
         <>
-            <button className='px-8 py-4 rounded-md border-2 border-blue-400 bg-slate-950 duration-100 blueShadow'>{children}</button>
+            <button className='m-auto px-8 py-4 rounded-md border-2 border-blue-400 bg-slate-950 duration-100 blueShadow'>{children}</button>
         </>
     )
 }
